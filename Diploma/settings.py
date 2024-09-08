cab8b585a34d70af6def8d37c388dc6868433c2f
dimtakos <dimitako@ihu.gr>
2024-09-08 20:23:51 +0300

    Database creation

--- Diploma/settings.py
+++ Diploma/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--7dq@x4@x=&kll1tint1yb!y5i#qs1*ui^)c(qve&&m)=0x8te
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
