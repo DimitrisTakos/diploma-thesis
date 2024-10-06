7ea19f15311f401dfe86479ad14fbc7d14df5120
dimtakos <dimitako@ihu.gr>
2024-10-06 12:59:52 +0300

    Creation weather page

--- tourism/views.py
+++ tourism/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import urllib.request
+import json
 
 # Create your views here.
 
@@ -7,3 +9,26 @@ def main_page(request):
     return render(request, 'tourism/main_page.html', {})
 
 
+def weather(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city +
+                                        '&units=metric&appid=99a93f520cea0aba11d7bf3bb4df5c17').read()
+
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            "main": str(list_of_data['weather'][0]['main']),
+            "description": str(list_of_data['weather'][0]['description']),
+            "icon": str(list_of_data['weather'][0]['icon']),
+        }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, "tourism/weather.html", data)
